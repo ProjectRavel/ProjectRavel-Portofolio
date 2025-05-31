5f85665ec3de2d1d52c85c7a518c3465844f046c
ProjectRavel <rafaelcodeid@gmail.com>
2025-06-01 06:10:18 +0700

    Refactor Home component layout for improved responsiveness and styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,11 @@ import Hero from "@/components/hero";
 import Project from "@/components/project";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[var(--background)] color-[var(--foreground)]">
-      <Navbar />
-      <Hero />
-      <Project />
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-start px-4 sm:px-8 md:px-16 lg:px-24 bg-[var(--background)] text-[var(--foreground)] overflow-x-hidden">
+  <Navbar />
+  <Hero />
+  <Project />
+</main>
+
   );
 }
