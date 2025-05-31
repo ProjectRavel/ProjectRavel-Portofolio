9296a16e3be18c48ffe50006e00fca56cd4db0c8
ProjectRavel <rafaelcodeid@gmail.com>
2025-06-01 03:01:19 +0700

    Add Project component to Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/navbar";
 import Hero from "@/components/hero";
+import Project from "@/components/project";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[var(--background)] color-[var(--foreground)]">
       <Navbar />
       <Hero />
+      <Project />
     </main>
   );
 }
