8f55980cba6682d02e9f8ca99148a1a5c16d6b4e
ProjectRavel <rafaelcodeid@gmail.com>
2025-06-01 03:01:34 +0700

    Add Project component to display project information on the Project Page

---
+++ components/project.tsx
@@ -0,0 +1,10 @@
+export default function Project() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-4xl font-bold mb-8">Project Page</h1>
+        <p className="text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, dolore illum! Recusandae velit, aliquam, ad ipsum tenetur, placeat unde fuga doloribus earum natus optio excepturi suscipit delectus dignissimos accusamus quibusdam?
+        </p>
+        </div>
+    );
+}
