41c6f8956daf4c5ba06964991d66633d2890f71a
ProjectRavel <rafaelcodeid@gmail.com>
2025-06-01 09:38:56 +0700

    Add About component to Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/navbar";
 import Hero from "@/components/hero";
+import About from "@/components/about";
 import Project from "@/components/project";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start px-4 sm:px-8 md:px-16 lg:px-24 bg-[var(--background)] text-[var(--foreground)] overflow-x-hidden">
+    <main className="flex min-h-screen flex-col items-center justify-start bg-[var(--background)] text-[var(--foreground)] overflow-x-hidden">
   <Navbar />
   <Hero />
+  <About />
   <Project />
 </main>
 
