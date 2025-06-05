4d7b90912a9e59a5bfbf5851214a2d041e028b8a
ProjectRavel <rafaelcodeid@gmail.com>
2025-06-05 14:05:21 +0700

    Add skill data for various technologies with descriptions and images

---
+++ app/data/skill-data.ts
@@ -0,0 +1,46 @@
+export const skills = [
+    {
+        title: "HTML",
+        description:
+        "Skilled in HTML5, with a focus on semantic markup, accessibility standards, and responsive design principles. Experienced in structuring web content effectively.",
+        image: "/images/html-icon.png"
+    },
+    {
+        title: "CSS",
+        description:
+        "Experienced in CSS3, including Flexbox and Grid layouts, animations, and responsive design techniques. Proficient in creating visually appealing and user-friendly interfaces.",
+        image: "/images/css-icon.png"
+    },
+    {
+        title: "Tailwind CSS",
+        description:
+        "Proficient in Tailwind CSS for utility-first styling. Experienced in building responsive and modern web interfaces with a focus on performance and maintainability.",
+        image: "/images/tailwind-icon.png"
+    },
+    {
+        title: "JavaScript",
+        slug: "javascript",
+        description:
+        "Proficient in JavaScript, including ES6+ features, asynchronous programming, and DOM manipulation. Experienced in building interactive web applications and understanding of JavaScript's event-driven architecture.",
+        image: "/images/javascript-icon.png"
+    },
+    {
+        title: "React",
+        description:
+        "Proficient in React.js for building dynamic user interfaces. Experienced with hooks, state management, and component lifecycle methods. Familiar with React Router for navigation.",
+        image: "/images/react-icon.png"
+    },
+    {
+        title: "Next.js",
+        description:
+        "Experienced in Node.js for server-side development. Proficient in building RESTful APIs, handling asynchronous operations, and using Express.js for routing.",
+        image: "/images/nextjs-icon.png"
+    },
+    {
+        title: "Laravel",
+        slug: "laravel",
+        description:
+        "Skilled in Laravel for backend development. Experienced with Eloquent ORM, routing, middleware, and building secure web applications.",
+        image: "/images/laravel-icon.png"
+    },
+]
